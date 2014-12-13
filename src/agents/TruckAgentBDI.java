fb70aa109e43c17ba95f9d53192d91921256292d
Tiago Coelho <tiagofscoelho@gmail.com>
2014-12-13 19:07:19 +0000

    - a ir pelo caminho mais curto para um deposito quando está a usar memoria ;  - alteracao na funcao de auto move

--- src/agents/TruckAgentBDI.java
+++ src/agents/TruckAgentBDI.java
@@ -13,8 +13,10 @@ import jadex.micro.annotation.*;
 import main.GarbageCollector;
 import main.Position;
 
+import java.lang.reflect.Array;
 import java.nio.channels.GatheringByteChannel;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -69,6 +71,7 @@ public class TruckAgentBDI {
     private int remainderCapacity = 0;
     private static ArrayList<Position> collectorsInMemory;
     private static ArrayList<Position> depositsInMemory;
+    private Position lastPos;
 
 
     /*------------------------------
@@ -78,9 +81,9 @@ public class TruckAgentBDI {
     public void init() {
         name = (String) agent.getArgument("Name");
         type = (GarbageCollector.typeOfWaste) agent.getArgument("Type");
-        if(type==null)
-            type=GarbageCollector.typeOfWaste.UNDIFFERENTIATED;
-        pos = new Position(0,0);
+        if (type == null)
+            type = GarbageCollector.typeOfWaste.UNDIFFERENTIATED;
+        pos = new Position(0, 0);
         pos.x = (Integer) agent.getArgument("PositionX");
         pos.y = (Integer) agent.getArgument("PositionY");
         capacity = (Integer) agent.getArgument("Capacity");
@@ -94,6 +97,7 @@ public class TruckAgentBDI {
         gc = GarbageCollector.getInstance().getInterface().getCity();
         collectorsInMemory = new ArrayList<>();
         depositsInMemory = new ArrayList<>();
+        lastPos = new Position(-1, -1);
     }
 
     @AgentBody
@@ -118,7 +122,7 @@ public class TruckAgentBDI {
         @GoalResult
         protected int r;
 
-        @GoalRecurCondition(beliefs="pause")
+        @GoalRecurCondition(beliefs = "pause")
         public boolean checkContext() {
             return !pause;
         }
@@ -131,7 +135,7 @@ public class TruckAgentBDI {
         @GoalResult
         protected int r;
 
-        @GoalRecurCondition(beliefs="pause")
+        @GoalRecurCondition(beliefs = "pause")
         public boolean checkContext() {
             return !pause;
         }
@@ -144,7 +148,7 @@ public class TruckAgentBDI {
         @GoalResult
         protected int r;
 
-        @GoalRecurCondition(beliefs="pause")
+        @GoalRecurCondition(beliefs = "pause")
         public boolean checkContext() {
             return !pause;
         }
@@ -154,16 +158,16 @@ public class TruckAgentBDI {
     /*-----------------------------
      Plans
     *---------------------------*/
-    @Plan(trigger=@Trigger(goals=WanderAroundCity.class))
-    protected void walkAroundCity() {
+    @Plan(trigger = @Trigger(goals = WanderAroundCity.class))
+    protected void walkAroundCity() throws InterruptedException {
 
-        do{
+        do {
             try {
                 Thread.sleep(SLEEP);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }while(pause = GarbageCollector.getInstance().getPause());
+        } while (pause = GarbageCollector.getInstance().getPause());
 
         try {
             Thread.sleep(SLEEP);
@@ -182,23 +186,23 @@ public class TruckAgentBDI {
         }
     }
 
-    @Plan(trigger=@Trigger(goals=CheckContainer.class))
+    @Plan(trigger = @Trigger(goals = CheckContainer.class))
     protected void checkContainer() {
 
-        do{
+        do {
             try {
                 Thread.sleep(SLEEP);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }while(pause = GarbageCollector.getInstance().getPause());
+        } while (pause = GarbageCollector.getInstance().getPause());
 
         ArrayList<Collector> adjacentsCollectors = GarbageCollector.getInstance().checkAdjacentCollectorPos(pos);
 
-        if(adjacentsCollectors.size() > 0){
-            for(int i = 0 ; i < adjacentsCollectors.size(); i++){
+        if (adjacentsCollectors.size() > 0) {
+            for (int i = 0; i < adjacentsCollectors.size(); i++) {
 
-                if(adjacentsCollectors.get(i).getType() == type) {
+                if (adjacentsCollectors.get(i).getType() == type) {
                     if (!collectorsInMemory.contains(adjacentsCollectors.get(i).getPosition()) && GarbageCollector.getInstance().getMemory()) {
                         collectorsInMemory.add(adjacentsCollectors.get(i).getPosition());
                         System.out.println("Adicionei a lista dos collectors que conheco o collector na posicao " + adjacentsCollectors.get(i).getPosition().x + "-" + adjacentsCollectors.get(i).getPosition().y);
@@ -225,37 +229,32 @@ public class TruckAgentBDI {
         }
     }
 
-    @Plan(trigger=@Trigger(goals=GoToDeposit.class))
+    @Plan(trigger = @Trigger(goals = GoToDeposit.class))
     protected void GoToDeposit() {
 
-        do{
+        do {
             try {
                 Thread.sleep(SLEEP);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }while(GarbageCollector.getInstance().getPause());
+        } while (GarbageCollector.getInstance().getPause());
 
-        ArrayList<Deposit> adjacentDeposits = GarbageCollector.getInstance().checkAdjacentDEpositPos(pos);
-
-        System.out.println("OccupiedCapacity: " + occupiedCapacity);
-        System.out.println("Capacity: " + capacity);
-        System.out.println("depositsInMemory.size(): " + depositsInMemory.size());
-        if(occupiedCapacity == capacity && depositsInMemory.size() > 0) {
-            System.out.println("Entrei para calcular o caminho mais curto");
+        if (occupiedCapacity == capacity && depositsInMemory.size() > 0 && !mission) {
             steps = getShortestPath(depositsInMemory);
-            System.out.println("ja tenho o caminho mais curto");
+            mission = true;
         }
 
+        ArrayList<Deposit> adjacentDeposits = GarbageCollector.getInstance().checkAdjacentDEpositPos(pos);
+        if (adjacentDeposits.size() > 0) {
 
-        if(adjacentDeposits.size() > 0) {
-
-            for(int i = 0 ; i < adjacentDeposits.size(); i++) {
+            for (int i = 0; i < adjacentDeposits.size(); i++) {
 
                 if (adjacentDeposits.get(i).getType() == type && occupiedCapacity != 0) {
                     adjacentDeposits.get(i).setOccupiedCapacity(adjacentDeposits.get(i).getOccupiedCapacity() + occupiedCapacity);
                     occupiedCapacity = 0;
-
+                    steps.clear();
+                    mission = false;
                     try {
                         Thread.sleep(SLEEP);
                     } catch (InterruptedException e) {
@@ -280,49 +279,60 @@ public class TruckAgentBDI {
         this.pause = pause;
     }
 
-    public void updatePos() {
-
-        System.out.println("Steps size : " + steps.size());
-
+    public void updatePos() throws InterruptedException {
 
         // have a trip to do
-       if (steps.size() > 0) {
+        if (steps.size() > 0) {
             pos = steps.remove(steps.size() - 1);
             System.out.println("Tou a fazer um passo presente em STEPS");
         } else {
-            pos = autoMove();
+            mission = false;
+            ArrayList<Position> neighbors = autoMove();
+            Position aux;
+            if (neighbors.size() == 1) {
+                lastPos = pos;
+                pos = neighbors.get(0);
+            }
+            else {
+                do {
+                    aux = neighbors.get(((int) (Math.random() * 1000) % neighbors.size()));
+                } while (aux.equals(lastPos));
+                lastPos = pos;
+                pos = aux;
+            }
         }
-
     }
 
-    public Position autoMove() {
+    public ArrayList<Position> autoMove() {
         ArrayList<Position> neighbors = new ArrayList<>();
-        if( isConnected(pos, new Position(pos.x, pos.y - 1)) ) neighbors.add(new Position(pos.x, pos.y - 1));
-        if( isConnected(pos, new Position(pos.x + 1, pos.y)) ) neighbors.add(new Position(pos.x + 1, pos.y));
-        if( isConnected(pos, new Position(pos.x, pos.y + 1)) ) neighbors.add(new Position(pos.x, pos.y + 1));
-        if( isConnected(pos, new Position(pos.x - 1, pos.y)) ) neighbors.add(new Position(pos.x - 1, pos.y));
+        if (isConnected(pos, new Position(pos.x, pos.y - 1))) neighbors.add(new Position(pos.x, pos.y - 1));
+        if (isConnected(pos, new Position(pos.x + 1, pos.y))) neighbors.add(new Position(pos.x + 1, pos.y));
+        if (isConnected(pos, new Position(pos.x, pos.y + 1))) neighbors.add(new Position(pos.x, pos.y + 1));
+        if (isConnected(pos, new Position(pos.x - 1, pos.y))) neighbors.add(new Position(pos.x - 1, pos.y));
 
-        return neighbors.get(((int)(Math.random() * 1000) % neighbors.size()));
+        //if(neighbors.size() == 1)
+        return neighbors;
+        // else
+        // return neighbors.get(((int)(Math.random() * 1000) % neighbors.size()));
+    }
+
+    public boolean PositionComparison(Position pos1, Position pos2) {
+        return pos1.equals(pos2);
     }
 
     public boolean isRoad(Position pos) {
-<<<<<<< HEAD
-        System.out.println("Tou aqui");
-        return gc.getCtB().getVertexByCoords(pos.x, pos.y).getName().charAt(0) == 'v';
-=======
         Vertex v = gc.getCtB().getVertexByCoords(pos.x, pos.y);
-        if( v != null )
+        if (v != null)
             return v.getName().charAt(0) == 'v';
         else
             return false;
->>>>>>> c113eea074d5a0520dccfb80b5c1e07979603914
     }
 
     public boolean isConnected(Position orig, Position dest) {
         Vertex origin = gc.getCtB().getVertexByCoords(orig.x, orig.y);
         Vertex destination = gc.getCtB().getVertexByCoords(dest.x, dest.y);
 
-       return gc.getCtB().getGraph().getEdge(origin, destination) != null;
+        return gc.getCtB().getGraph().getEdge(origin, destination) != null;
     }
 
     public Position getPosition() {
@@ -334,60 +344,57 @@ public class TruckAgentBDI {
         return type;
     }
 
-    public ArrayList<Position> getShortestPath(Position dest){
+    public ArrayList<Position> getShortestPath(Position dest) {
         System.out.println("3 - \n" + dest.toString());
         ArrayList<Position> path = new ArrayList<>();
 
-        System.out.println("creating neighbors");
         ArrayList<Position> neighbors = new ArrayList<>();
 
-        System.out.println("checking road 1");
-        if( isRoad(new Position(dest.x, dest.y - 1)) )
+        if (isRoad(new Position(dest.x, dest.y - 1)))
             neighbors.add(new Position(dest.x, dest.y - 1));
-        System.out.println("checking road 2");
-        if( isRoad(new Position(dest.x + 1, dest.y)) )
+
+        if (isRoad(new Position(dest.x + 1, dest.y)))
             neighbors.add(new Position(dest.x + 1, dest.y));
-        System.out.println("checking road 3");
-        if( isRoad(new Position(dest.x, dest.y + 1)) )
+
+        if (isRoad(new Position(dest.x, dest.y + 1)))
             neighbors.add(new Position(dest.x, dest.y + 1));
-        System.out.println("checking road 4");
-        if( isRoad(new Position(dest.x - 1, dest.y)) )
+
+        if (isRoad(new Position(dest.x - 1, dest.y)))
             neighbors.add(new Position(dest.x - 1, dest.y));
 
-        System.out.println("4");
         int min = Integer.MAX_VALUE;
         int minIndex = 0;
         int i = 0;
-        for( Position p : neighbors ){
-            int pSize = DijkstraShortestPath.findPathBetween( gc.getCtB().getGraph(), gc.getCtB().getVertexByCoords(pos.x, pos.y), gc.getCtB().getVertexByCoords(p.x, p.y)).size();
-            if( pSize < min ){
+        for (Position p : neighbors) {
+            int pSize = DijkstraShortestPath.findPathBetween(gc.getCtB().getGraph(), gc.getCtB().getVertexByCoords(pos.x, pos.y), gc.getCtB().getVertexByCoords(p.x, p.y)).size();
+            if (pSize < min) {
                 min = pSize;
                 minIndex = i;
             }
             i++;
         }
 
-        List<DefaultEdge> stepsEdges = DijkstraShortestPath.findPathBetween( gc.getCtB().getGraph(), gc.getCtB().getVertexByCoords(pos.x, pos.y), gc.getCtB().getVertexByCoords(neighbors.get(minIndex).x, neighbors.get(minIndex).y));
-        for( DefaultEdge e : stepsEdges ){
+        List<DefaultEdge> stepsEdges = DijkstraShortestPath.findPathBetween(gc.getCtB().getGraph(), gc.getCtB().getVertexByCoords(pos.x, pos.y), gc.getCtB().getVertexByCoords(neighbors.get(minIndex).x, neighbors.get(minIndex).y));
+        for (DefaultEdge e : stepsEdges) {
             path.add(gc.getCtB().getGraph().getEdgeTarget(e).getPosition());
         }
+
+        Collections.reverse(path);
 
         return path;
     }
 
-    public ArrayList<Position> getShortestPath(ArrayList<Position> dest){
+    public ArrayList<Position> getShortestPath(ArrayList<Position> dest) {
 
-        System.out.println("1");
         ArrayList<ArrayList<Position>> paths = new ArrayList<>();
         int min = Integer.MAX_VALUE;
         int minIndex = 0;
         int i = 0;
 
-        System.out.println("2");
-        for( Position p : dest ){
-            paths.add( this.getShortestPath( p ) );
-            if( paths.get(paths.size()-1).size() < min ){
-                min = paths.get(paths.size()-1).size();
+        for (Position p : dest) {
+            paths.add(this.getShortestPath(p));
+            if (paths.get(paths.size() - 1).size() < min) {
+                min = paths.get(paths.size() - 1).size();
                 minIndex = i;
             }
             i++;
